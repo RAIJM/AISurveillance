2cced991ee0fe26c6670bed34b8170e96d35ecd7
Stone <generalms@hotmail.com>
2018-10-18 11:28:33 -0500

    add object detection code

---
+++ object_detector.py
@@ -0,0 +1,81 @@
+import numpy as np
+import os
+import cv2
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+CWD_PATH = os.getcwd()
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+PATH_TO_CKPT = os.path.join(CWD_PATH, 'object_detection', MODEL_NAME, 'frozen_inference_graph.pb')
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join(CWD_PATH, 'object_detection', 'data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+# Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+
+def detect_objects(image_np,sess,detection_graph):
+	image_np_expanded = np.expand_dims(image_np, axis=0)
+	image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+	# Each box represents a part of the image where a particular object was detected.
+	boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+	# Each score represent how level of confidence for each of the objects.
+	# Score is shown on the result image, together with the class label.
+	scores = detection_graph.get_tensor_by_name('detection_scores:0')
+	classes = detection_graph.get_tensor_by_name('detection_classes:0')
+	num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+	# Actual detection.
+	(boxes, scores, classes, num_detections) = sess.run(
+		[boxes, scores, classes, num_detections],
+		feed_dict={image_tensor: image_np_expanded})
+
+	# Visualization of the results of a detection.
+	vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+	return image_np
+
+if __name__ == '__main__':
+	cap = cv2.VideoCapture(0) #front camera
+
+	detection_graph = tf.Graph()
+	with detection_graph.as_default():
+		od_graph_def = tf.GraphDef()
+		with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+			serialized_graph = fid.read()
+			od_graph_def.ParseFromString(serialized_graph)
+			tf.import_graph_def(od_graph_def, name='')
+
+		sess = tf.Session(graph=detection_graph) #create a tensorflow session
+		
+	while(cap.isOpened()):
+		ret, frame = cap.read() #read frame from camera
+		if ret==True:
+			image = detect_objects(frame,sess,detection_graph)
+			cv2.imshow('frame',image) #show image
+			if cv2.waitKey(1) & 0xFF == ord('q'): #exit if q is pressed
+				break
+		else:
+			break
+
+	# Release everything if job is finished
+	cap.release()
+	cv2.destroyAllWindows()
+
